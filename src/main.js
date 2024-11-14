5447bd681bcd25ddb6f8e10c8ccb166da0d1ff11
eyu birhanu <151485420+Eyu32@users.noreply.github.com>
2024-11-14 20:47:41 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
         $('#logo').toggleClass('active-header-img')
         $('#hamburger').toggleClass('active-header-img active-hamburger')
         if(!isActive){
-            $('#hamburger img').attr('src', '../images/icon-close.svg')
+            $('#hamburger img').attr('src', './images/icon-close.svg')
             isActive = true
         }else if (isActive){
-            $('#hamburger img').attr('src', '../images/icon-hamburger.svg')
+            $('#hamburger img').attr('src', './images/icon-hamburger.svg')
             isActive = false
         }
         
