cd588c64532d2b36791dcb9e415022cb513fcff6
eyu birhanu <151485420+Eyu32@users.noreply.github.com>
2024-12-08 19:01:32 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
         'Josefin-sans': '"Josefin Sans", sans-serif'
       },
       backgroundImage:{
-        'hero-mobile': "url('./images/mobile/image-hero.jpg')",
-        'hero-desktop': "url('./images/desktop/image-hero.jpg')"
+        'hero-mobile': "url('./images/image-mobile-hero.jpg')",
+        'hero-desktop': "url('./images/image-desktop-hero.jpg')"
       },
       colors: {
         'Dark-Gary': 'hsl(0, 0%, 55%)',
